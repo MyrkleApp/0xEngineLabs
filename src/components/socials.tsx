fd5fb940afbde42f6b80b7e85ed0b8baa545af00
Samuel Oziegbe <sammyoziegbe@gmail.com>
2026-01-17 20:03:06 +0100

    added facebook link

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,15 +1,15 @@
 import { HStack } from "@chakra-ui/react";
 import LinkedInIcon from "./icons/linked-in";
-// import FacebookIcon from "./icons/facebook";
+import FacebookIcon from "./icons/facebook";
 // import YoutubeIcon from "./icons/youtube";
 // import InstagramIcon from "./icons/instagram";
 
 function Socials() {
   return (
     <HStack fontSize="2xl" gap={6}>
-      {/* <a href="#">
+      <a href="https://www.facebook.com/0xenginelabs" target="_blank" rel="noopener noreferrer">
         <FacebookIcon />
-      </a> */}
+      </a>
       <a href="https://www.linkedin.com/company/0xenginelabs/" target="_blank" rel="noopener noreferrer">
         <LinkedInIcon />
       </a>
